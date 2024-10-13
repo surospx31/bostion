a1229773551a9fc3cab4063fab973ac2f2decd78
surospx31 <romanukd445@gmail.com>
2024-10-13 23:28:00 +0300

    Додав початкову версію веб-додатку

--- app.js
+++ app.js
@@ -24,11 +24,32 @@ if (refCode) {
     console.warn('Реферальний код не знайдено у URL');
 }
 
-    
+    if (window.Telegram && window.Telegram.WebApp) {
+        const tg = window.Telegram.WebApp;
+        tg.expand();
+
+        if (tg.initDataUnsafe && tg.initDataUnsafe.user) {
+            userId = tg.initDataUnsafe.user.id;
+            name = tg.initDataUnsafe.user.first_name || tg.initDataUnsafe.user.username || 'Username';
+        } else {
+            console.error("Telegram WebApp не повертає дані користувача");
+        }
+
+        const userNicknameElement = document.getElementById('userNickname');
+        userNicknameElement.textContent = name;
+    }
+
+    if (!userId) {
+        console.error('Не вдалося отримати telegram_id користувача');
+        return;
+    }
 
     document.addEventListener('touchmove', function(event) {
         event.preventDefault();
     }, { passive: false });
+
+
+    
     const getButterflyButton = document.getElementById('getButterflyButton');
     const welcomeSection = document.getElementById('welcome');
     const butterflySection = document.getElementById('butterflySection');
