7132d3128449a603d1f45a59ad44c179577fad11
surospx31 <romanukd445@gmail.com>
2024-10-16 02:44:20 +0300

    Додав початкову версію веб-додатку

--- api/user/[telegram_id].js
+++ api/user/[telegram_id].js
@@ -95,3 +95,30 @@ app.post('/api/user/:telegram_id', async (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+const TelegramBot = require('node-telegram-bot-api');
+
+// Токен бота
+const TOKEN = '6540307506:AAFlc4_ZMwmKScwO8mRaJrs38d-Dm3gB_d0';
+
+// Ініціалізація бота
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+// Обробник для команди /start без реферального коду
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+
+    // Привітальне повідомлення
+    bot.sendMessage(chatId, "Ласкаво просимо до бота!");
+
+    // Додаємо кнопку з посиланням на mini web app
+    const webAppUrl = "https://www.wellactcodepages.xyz/";
+    const opts = {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: "Відкрити додаток", web_app: { url: webAppUrl } }]
+            ]
+        }
+    };
+    bot.sendMessage(chatId, "Натисніть для відкриття додатку", opts);
+});
