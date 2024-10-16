1fcbf020bc782e98c07434a9d3cfdafd62a0ab32
surospx31 <romanukd445@gmail.com>
2024-10-17 01:56:08 +0300

    Додав початкову версію веб-додатку

--- app.js
+++ app.js
@@ -21,7 +21,29 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('Немає реферального коду, зберігаємо стандартний запис'); // Виклик без реферального коду
     }
 
-   
+    if (window.Telegram && window.Telegram.WebApp) {
+        const tg = window.Telegram.WebApp;
+        tg.expand();
+
+        if (tg.initDataUnsafe && tg.initDataUnsafe.user) {
+            userId = tg.initDataUnsafe.user.id;
+            name = tg.initDataUnsafe.user.first_name || tg.initDataUnsafe.user.username || 'Username';
+        } else {
+            console.error("Telegram WebApp не повертає дані користувача");
+        }
+
+        const userNicknameElement = document.getElementById('userNickname');
+        userNicknameElement.textContent = name;
+    }
+
+    if (!userId) {
+        console.error('Не вдалося отримати telegram_id користувача');
+        return;
+    }
+
+    document.addEventListener('touchmove', function(event) {
+        event.preventDefault();
+    }, { passive: false });
 
     
 
