449f1ee35d9fa633bb315152a8f4951d74f1ad9a
surospx31 <romanukd445@gmail.com>
2024-10-23 22:55:42 +0300

    Додав початкову версію веб-додатку

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ document.addEventListener('DOMContentLoaded', () => {
         progressElement.style.width = progress;
     }
     
-    async function saveUserDataWithReferral(startParam, name, userId) {
+    async function saveUserDataWithReferral(startParam) {
         try {
             await fetch(`/api/user/${userId}`, {
                 method: 'POST',
@@ -167,7 +167,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    async function saveUserData(name, userId) {
+    async function saveUserData() {
         console.log('Зберігаємо дані користувача...');
         try {
             await fetch(`/api/user/${userId}`, {
